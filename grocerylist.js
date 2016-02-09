389453f2d2b4c3626bf83b9ca7a2021ec53ee468
Tom <tomalexanderphoto@gmail.com>
2016-02-09 14:53:38 -0500

    finished grocery list js program

---
+++ grocerylist.js
@@ -0,0 +1,33 @@
+var total = 0;
+var groceryList = [
+	{
+		name: 'apples',
+		price: 2
+	},
+	{
+		name: 'cereal',
+		price: 5
+	},
+	{
+		name: 'eggs',
+		price: 3
+	},
+	{
+		name: 'milk',
+		price: 2.50
+	},
+	{
+		name: 'cheese',
+		price: 7
+	},
+];
+
+for (i = 0; i < groceryList.length; i++) {
+	total += groceryList[i].price;
+}
+
+groceryList.forEach(function(item, index, array) {
+	console.log(item.name + " $" + item.price);
+});
+
+console.log("total = $" + total);
