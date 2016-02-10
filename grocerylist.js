56c71142ea9729f89079faef20fe157c691e4651
Tom <tomalexanderphoto@gmail.com>
2016-02-10 09:26:40 -0500

    small edit

--- grocerylist.js
+++ grocerylist.js
@@ -1,5 +1,6 @@
 var total = 0;
 var groceryList = [
+
 	{
 		name: 'apples',
 		price: 2
@@ -20,13 +21,18 @@ var groceryList = [
 		name: 'cheese',
 		price: 7
 	},
+
 ];
 
 for (i = 0; i < groceryList.length; i++) {
 	total += groceryList[i].price;
 }
 
-groceryList.forEach(function(item, index, array) {
+/* groceryList.forEach(function(item, index, array) {
+	console.log(item.name + " $" + item.price);
+}); */
+
+groceryList.forEach(function(item) {
 	console.log(item.name + " $" + item.price);
 });
 
